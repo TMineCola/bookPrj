3e342a16e1df657bab7a3a8b0094b6c9e3302536
sky <kenornotes@gmail.com>
2017-11-15 10:00:15 +0800

    add insert comment in crontrol.php

--- control.php
+++ control.php
@@ -29,6 +29,11 @@ case 'like':
 		likeBook($id);
 	}
 	break;
+case 'insertComment':
+	$bkID=(int)$_REQUEST['bkID'];
+	$msg=$_REQUEST['msg'];
+	insertComment($bkID, $msg, $_SESSION['uID']);
+	break;
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
