503c8c56e3460bd345241291b0f80b51391633a4
TMineCola <s104213083@mail1.ncnu.edu.tw>
2017-11-18 00:08:26 +0800

    Prettify, 新增註解

--- control.php
+++ control.php
@@ -2,45 +2,63 @@
     session_start();
     require_once('model.php');
     require_once('loginModel.php');
+    //除了依據請求方式得到資料如 $_GET['名稱'] or $_POST['名稱'], 可以使用$_REQUEST['名稱']來獲得資料, 而且不用區分方法
+    //將請求的行為存入action
     $action =$_REQUEST['act'];
-
+    //判斷行為呼叫對應的功能
     switch ($action) {
-    case 'delete':
-    $id = (int) $_REQUEST['id'];
-        if ($id > 0) {
-            deleteBook($id);
-        }
-        break;
-    case 'insert':
-        $title=$_REQUEST['title'];
-        $msg=$_REQUEST['msg'];
-        $name=$_REQUEST['myname'];
-        insertBook($title, $msg, $name, $_SESSION['uID']);
-        break;
-    case 'update':
-        $id = (int) $_REQUEST['id'];
-        $title=$_REQUEST['title'];
-        $msg=$_REQUEST['msg'];
-        $name=$_REQUEST['myname'];
-        updateBook($id, $title, $msg, $name);
-        break;
-    case 'like':
-        $id = (int) $_REQUEST['id'];
-        if ($id > 0) {
-            likeBook($id);
-        }
-        break;
-    case 'insertComment':
-        $bkID=(int)$_REQUEST['bkID'];
-        $msg=$_REQUEST['msg'];
-        insertComment($bkID, $msg, $_SESSION['uID']);
-        break;
-    case 'deleteComment':
-        $id = (int) $_REQUEST['id'];
-        if ($id > 0 and isAdmin($_SESSION['uID'])) {
-            deleteComment($id);
-        }
-        break;
+        case 'delete':
+            //從請求中讀取id ,對id做基本錯誤檢查(強制轉成數字)
+            $id = (int) $_REQUEST['id'];
+            //基本錯誤檢查, 正常id一定會 > 0
+            if ($id > 0) {
+                //刪除該編號(id)推薦書單
+                deleteBook($id);
+            }
+            break;
+        case 'insert':
+            //從請求中讀取資料
+            $title = $_REQUEST['title'];
+            $msg = $_REQUEST['msg'];
+            $name = $_REQUEST['myname'];
+            //新增推薦書單
+            insertBook($title, $msg, $name, $_SESSION['uID']);
+            break;
+        case 'update':
+            //從請求中讀取id ,對id做基本錯誤檢查(強制轉成數字)
+            $id = (int) $_REQUEST['id'];
+            //從請求中讀取資料
+            $title = $_REQUEST['title'];
+            $msg = $_REQUEST['msg'];
+            $author = $_REQUEST['author'];
+            //修改推薦書單
+            updateBook($id, $title, $msg, $author);
+            break;
+        case 'like':
+            //從請求中讀取id ,對id做基本錯誤檢查(強制轉成數字)
+            $id = (int) $_REQUEST['id'];
+            //基本錯誤檢查, 正常id一定會 > 0
+            if ($id > 0) {
+                likeBook($id);
+            }
+            break;
+        case 'insertComment':
+            //從請求中讀取bkID ,對bkID做基本錯誤檢查(強制轉成數字)
+            $bkID = (int)$_REQUEST['bkID'];
+            //從請求中讀取資料
+            $msg = $_REQUEST['msg'];
+            //新增回應
+            insertComment($bkID, $msg, $_SESSION['uID']);
+            break;
+        case 'deleteComment':
+            //從請求中讀取id ,對id做基本錯誤檢查(強制轉成數字)
+            $id = (int) $_REQUEST['id'];
+            //基本錯誤檢查, 正常id一定會 > 0 及 檢查該登入使用者是否為管理員
+            if ($id > 0 and isAdmin($_SESSION['uID'])) {
+                //檢查通過則刪除回應
+                deleteComment($id);
+            }
+            break;
     }
 ?>
 
@@ -48,9 +66,10 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>無標題文件</title>
+        <title>推薦書單系統</title>
     </head>
     <body>
-        <a href='view.php'>執行完成，回留言板</a>
+        <!-- 請求執行完畢後顯示回主畫面的連結 -->
+        <a href='view.php'>執行完成，回所有推薦書單瀏覽</a>
     </body>
 </html>
--- loginForm.php
+++ loginForm.php
@@ -1,14 +1,13 @@
 <?php
-session_start();
-//require("dbconnect.php");
-
-//set the login mark to empty
-$_SESSION['uID'] = 0;
+    session_start();
+    //將使用者的登入狀態設為 0
+    $_SESSION['uID'] = 0;
 ?>
-<h1>Login Form</h1><hr />
+<h1>登入系統</h1><hr />
+<!-- 將使用者的帳密以post的方式送至 loginControl.php 進行處理 -->
 <form method="post" action="loginControl.php">
-<input type="hidden" name="act" value="login">
-User Name: <input type="text" name="id"><br />
-Password : <input type="password" name="pwd"><br />
-<input type="submit">
+    <input type="hidden" name="act" value="login">
+    帳號: <input type="text" name="id"><br />
+    密碼 : <input type="password" name="pwd"><br />
+    <input type="submit">
 </form>
